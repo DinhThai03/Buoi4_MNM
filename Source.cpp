29fc068d504a538af1ad9b6775ce93b959453800
DinhThai03 <thai.05.10.2001@gmail.com>
2024-11-02 10:43:01 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -4,10 +4,24 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b)
+{
+	if(b != 0)
+		return a/b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a, b;
+	cout << "nhap a: ";
+	cin >> a;
+	cout << "nhap b: "
+	cin >> b;
+	
+	if(b != 0)
+		cout << "Thuong " << a << "/" << b << "la: " << thuong(a,b);
+	else
+		cout << "Khong the chi vi b = 0";
 
 	system("pause");
 	return 0;
