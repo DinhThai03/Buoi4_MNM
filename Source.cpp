1bffac94d1438006933a682e4e8352540c2041df
Voxuanhuy230903 <xuanhuy230903@gmail.com>
2024-11-02 11:25:58 +0700

    tong

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b){
-	return a*b;
-float thuong(int a, int b)
+int tong(int a, int b)
 {
-	if(b != 0)
-		return a/b;
+	return a+b;
 }
+int hieu(int a, int b);
+int tich(int a, int b);
+float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
@@ -18,12 +16,7 @@ int main()
 	cin >> a;
 	cout << "nhap b: "
 	cin >> b;
-	cout<<"Tich la"<<tich(a,b);
-	if(b != 0)
-		cout << "Thuong " << a << "/" << b << "la: " << thuong(a,b);
-	else
-		cout << "Khong the chi vi b = 0";
-	
+	cout << "Tong la: " << tong(a,b) << endl;
 	system("pause");
 	return 0;
 }
