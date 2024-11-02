8b11a9f3dad22ec83955954258d27f920cb41e83
YasuoTri <phamminhtri26102003@gmail.com>
2024-11-02 10:43:55 +0700

    thay doi tich

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	return a*b;
+};
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout<<"Tich la"<<tich(2,2);
 	system("pause");
 	return 0;
 }
